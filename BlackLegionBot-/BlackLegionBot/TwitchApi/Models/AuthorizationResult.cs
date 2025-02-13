1659b5417e4933fccefe0bdee69030900acde8f8
Damian M. Buzink <dbuzink@gmail.com>
2025-02-13 21:13:30 +0100

    Add additional json attributes?

--- BlackLegionBot-/BlackLegionBot/TwitchApi/Models/AuthorizationResult.cs
+++ BlackLegionBot-/BlackLegionBot/TwitchApi/Models/AuthorizationResult.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace BlackLegionBot.TwitchApi.Models
@@ -6,13 +7,17 @@ namespace BlackLegionBot.TwitchApi.Models
     public class AuthorizationResult
     {
         [JsonProperty("access_token")]
+        [JsonPropertyName("access_token")]
         public string AccessToken { get; set; }
         [JsonProperty("refresh_token")]
+        [JsonPropertyName("refresh_token")]
         public string RefreshToken { get; set; }
         [JsonProperty("expires_in")]
+        [JsonPropertyName("expires_in")]
         public int ExpiresIn { get; set; }
         public IEnumerable<string> Scopes { get; set; }
         [JsonProperty("token_type")]
+        [JsonPropertyName("token_type")]
         public string TokenType { get; set; }
 
     }
