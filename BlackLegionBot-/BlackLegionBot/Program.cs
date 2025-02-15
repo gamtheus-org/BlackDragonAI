ad352b4f6b79722a69b8c4438fcf7909020daacb
Damian M. Buzink <dbuzink@gmail.com>
2025-02-15 20:39:55 +0100

    Add config

--- BlackLegionBot-/BlackLegionBot/Program.cs
+++ BlackLegionBot-/BlackLegionBot/Program.cs
@@ -8,6 +8,7 @@ using BlackLegionBot.TwitchApi;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using Refit;
 
 namespace BlackLegionBot
@@ -28,6 +29,7 @@ namespace BlackLegionBot
 
                     // configs
                     var blbApiConfig = new BlbApiConfig(config.GetSection("Blbapi"));
+                    Console.WriteLine($"Config: {JsonConvert.SerializeObject(blbApiConfig)}");
                     services.AddSingleton<BlbApiConfig>(blbApiConfig);
                     var ircCredentials = new IrcCredentials(config.GetSection("Irc"));
                     services.AddSingleton(ircCredentials);
