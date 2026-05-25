99e4c155f01ea9000bca0122614c8acb35ac7db2
Gamtheus <dbuzink@gmail.com>
2026-05-25 21:11:18 +0200

    Add "sᴛʀeᴀᴍʙᴏᴏ" as banned term

--- BlackLegionBot-/BlackLegionBot/Helpers/SpamChecker.cs
+++ BlackLegionBot-/BlackLegionBot/Helpers/SpamChecker.cs
@@ -11,7 +11,7 @@ namespace BlackLegionBot.Helpers;
 public class SpamChecker : IMessageValidator
 {
     private const string BanReason = "banned for spam";
-    private static readonly string[] BannedTerms = ["streamboo", "nezhna"];
+    private static readonly string[] BannedTerms = ["streamboo", "nezhna", "sᴛʀeᴀᴍʙᴏᴏ"];
 
     private readonly TwitchApiManager _twitchApiManager;
 
